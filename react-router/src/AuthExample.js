fbbe733e7f20e06e7bfa69f42a7abc7ed5e8362e
Allen Zhao <995029442@qq.com>
2017-12-28 23:35:08 +0800

    read API about react-router v4

--- react-router/src/AuthExample.js
+++ react-router/src/AuthExample.js
@@ -10,7 +10,7 @@ import {
 /*
 => 自定义了一个 PrivateRoute 组件, 这也是 Route 组件的一种用法 wrapping/composing
     => 登录就渲染 Protected 组件
-    => 否则的话跳转到 '/login' 路由
+    => 否则的话跳转到 '/login' 路由, '/login' 路由匹配 Login 组件
 => 
 */
 
@@ -53,11 +53,10 @@ const AuthButton = withRouter(({ history }) => (
     )
 ))
 
-// history 是什么？
+// history 是什么?
 // Redirect 中的 to 的对象, state 就是在这边的传递里的 rest 就是属性中的对象
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-    console.log(rest);
     return (
         <Route {...rest} render={ props => (
             fakeAuth.isAuthenticated ? (
@@ -75,7 +74,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 const Public = () => <h3>Public</h3>
 const Protected = () => <h3>Protected</h3>
 
-// 定义组件现在还能这么用了？
+// 定义组件现在还能这么用了?
 class Login extends React.Component {
     state = {
         redirectToReferrer: false
@@ -88,6 +87,7 @@ class Login extends React.Component {
     }
 
     render() {
+        console.log(this.props.location);
         const { from } = this.props.location.state || { from: { pathname: '/' } };
         const { redirectToReferrer } = this.state;
 
